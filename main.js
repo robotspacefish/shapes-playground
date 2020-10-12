bffd06aeffe8dcd7c1c8f24510e159b0d297ea32
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-10-12 19:09:59 -0400

    add mouse location cursor

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
+// ====== GLOBAL CONSTANTS ==================================
 const ctx = document.getElementById('js-canvas').getContext('2d');
 const canvasContainer = document.getElementById('js-canvas-container');
 const WIDTH = ctx.canvas.width, HEIGHT = ctx.canvas.height;
+const mouse = { x: null, y: null, textX: null, texY: null };
+
+
+
+
+function draw() {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+  if (mouse.x && mouse.y) drawMouseCoords();
+
+  requestAnimationFrame(draw);
+}
+
+// ====== HELPERS ============================================
+function drawMouseCoords() {
+  ctx.lineWidth = 2;
+  ctx.fillStyle = 'red';
+  ctx.strokeStyle = 'red';
+  ctx.fillText(`(x: ${mouse.x}, y: ${mouse.y})`, mouse.textX, mouse.textY);
+
+  // cursor
+  ctx.beginPath();
+  ctx.moveTo(mouse.x - 10, mouse.y);
+  ctx.lineTo(mouse.x + 10, mouse.y);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(mouse.x, mouse.y - 10);
+  ctx.lineTo(mouse.x, mouse.y + 10);
+  ctx.stroke();
+}
 
 function resize() {
   let cWidth = canvasContainer.clientWidth,
@@ -21,6 +52,7 @@ function resize() {
   ctx.canvas.style.height = `${cHeight}px`;
 }
 
+// ====== EVENT LISTENERS ===================================
 window.addEventListener('load', () => {
   resize();
 })
@@ -28,3 +60,28 @@ window.addEventListener('load', () => {
 window.addEventListener('resize', () => {
   resize();
 })
+
+ctx.canvas.addEventListener('mousemove', (e) => {
+  const rect = ctx.canvas.getBoundingClientRect(),
+    scaleX = ctx.canvas.width / rect.width,
+    scaleY = ctx.canvas.height / rect.height;
+
+  mouse.x = Math.floor((e.clientX - rect.left) * scaleX);
+  mouse.y = Math.floor((e.clientY - rect.top) * scaleY);
+  mouse.textX = mouse.x;
+  mouse.textY = mouse.y - 10;
+
+  // move text if too close to edges
+  mouse.textX += mouse.x > ctx.canvas.width - 45 ? -70 : 10;
+
+  if (mouse.y > ctx.canvas.height - 30) {
+    mouse.textY = mouse.y - 20;
+  } else if (mouse.y < 30) {
+    mouse.textY = mouse.y + 20;
+  } else {
+    mouse.textY -= 10
+  }
+})
+
+// ====== START ==============================================
+draw();
